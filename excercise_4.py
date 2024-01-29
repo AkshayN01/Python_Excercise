69bed04c23be8947a4f0d7a35adc7d4d30f1548e
Akshay Nambly <nambly77@gmail.com>
2024-01-29 21:42:46 +0530

    Updated excercise 4

--- excercise_4.py
+++ excercise_4.py
@@ -1,7 +1,9 @@
 def getSumOfAllMultiples(num1, num2, limit):
     total = 0
     for i in range(limit):
-        if (i%num1 == 0) OR (i%num2 == 0):
+        if (i%num1 == 0) or (i%num2 == 0):
             total += i
 
     return total
+
+print(getSumOfAllMultiples(3,5,1000))

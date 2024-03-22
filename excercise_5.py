8deb1d10076d130ac37f8b8f08f0301216f0e2d4
AkshayN01 <nambly77@gmail.com>
2024-03-22 12:57:38 +0000

    r=moved exercises 5 and 6 to folder

--- excercise_5.py
+++
@@ -1,9 +0,0 @@
-def getSumOfAllMultiples(num1, num2, num_list):
-    total = 0
-    for i in num_list:
-        if ((i%num1 == 0) or (i%num2 == 0)):
-            total += i
-
-    return total
-
-print(getSumOfAllMultiples(3,5,[1,2,3,4,5,6,7,8,9,10]))
--- excercise_6.py
+++
@@ -1,9 +0,0 @@
-def getDivisors(n, num_list):
-    for i in n:
-        total = 0
-        for j in num_list:
-            if(i%j == 0):
-                total += j
-        print(f"Sum of multiples of {i} is {total}")
-
-getDivisors([8,9],[1,2,3,4,5,6,7,8,9,10])
